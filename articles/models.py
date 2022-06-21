913fe823c8edeff9447955f9786beece506d6f1d
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-06-21 14:17:18 -0400

    created article model and added statuses

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Article(models.Model):
+    PHASES = [
+        ('DR', 'Draft'),
+        ('SU', 'Submitted'),
+        ('RE', 'Rejected'),
+        ('PU', 'Published'),
+        ('AR', 'Archived'),
+    ]
+    
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phase = models.CharField(max_length=10, choices=PHASES, default='DR')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
