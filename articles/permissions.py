5e32026dcd1becffd247f2d11b8baeda7fe92c24
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-06-22 12:25:19 -0400

    working on edit permissions

--- articles/permissions.py
+++ articles/permissions.py
@@ -2,21 +2,45 @@ from rest_framework import permissions
 
 class IsAuthor(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        
         if not request.user.is_authenticated:
             return False
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
 
         if not obj.phase in ['DR','RE']:
             return False
 
-        if not (request.data.get('phase') == obj.phase or request.data.get('phase') == 'SU'):
-            return False
+        if request.data.get('phase') in [obj.phase, 'SU']:
+            return True
     
-        return True
+        return False
 
 
 class IsEditor(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user.is_superuser
+        if not request.user.is_superuser:
+            return False
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if request.data.get('title') != obj.title:
+            return False
+
+        if request.data.get('body') != obj.body:
+            return False
+
+        if request.data.get('image') != obj.image:
+            return False
+
+        if request.data.get('category') != obj.category:
+            return False
+
+        if request.data.get('phase') == 'SU' and obj.phase in ['RE','PU']:
+            return True
+
+        if request.data.get('phase') == 'PU' and obj.phase == 'AR':
+            return True
+
+        return False
