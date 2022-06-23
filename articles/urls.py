9a6d7db096846fec0ea2d48fe14c24ce7e39fda3
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-06-23 17:22:04 -0400

    updated endpoints so one url can be used to view article detail

--- articles/urls.py
+++ articles/urls.py
@@ -5,10 +5,8 @@ app_name = 'articles'
 
 urlpatterns = [
     path('articles/review/<int:pk>/', ArticleUpdatePhaseView.as_view(), name='article_edit_phase'),
-    # path('articles/review/<int:pk>/', ArticleDetailApiViewEditor.as_view(), name='article_detail_editor'),
     path('articles/review/', ArticleListApiViewReview.as_view(), name='article_list_review'),
     path('articles/<int:pk>/', ArticleDetailApiView.as_view(), name='article_detail'),
-    # path('articles/mine/<int:pk>/', ArticleDetailApiView.as_view(), name='article_detail'),
     path('articles/mine/', ArticleListApiViewMine.as_view(), name='article_list_mine'),
     path('articles/', ArticleListApiView.as_view(), name='article_list'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics
 from .models import Article
 from .serializers import ArticleSerializer, UpdatePhaseSerializer
 from django.db.models import Q
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from articles.permissions import IsAuthorOrReadOnly, IsEditor
 
 
@@ -35,12 +35,6 @@ class ArticleListApiViewReview(generics.ListAPIView):
     queryset = Article.objects.filter(Q(phase='SU') | Q(phase='PU')).order_by('-created_at')
     serializer_class = ArticleSerializer
     permission_classes = (IsEditor,)
-
-
-# class ArticleDetailApiViewEditor(generics.RetrieveAPIView):
-#     queryset = Article.objects.all()
-#     serializer_class = ArticleSerializer
-#     permission_classes = (IsEditor,)
 
 
 class ArticleUpdatePhaseView(generics.UpdateAPIView):
